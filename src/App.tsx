44f1b1d797ae906086580ae94cd38194367aa0d8
jadson santos <jadsoonbol@gmail.com>
2024-07-12 15:31:56 -0300

    get snapshot chamados and build date chamado

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import Rotas from "./router";
-import { useEffect } from "react";
-import firebase from "firebase/compat/app";
-import { firebaseConfig } from "./firebaseConfig";
 
 const App = () => {
 
-    useEffect(() => {
-        if (!firebase.apps.length) {
-          firebase.initializeApp(firebaseConfig);
-        }
-        console.log('useEffect do app');
-      }, []);
+    // useEffect(() => {
+    //     if (!firebase.apps.length) {
+    //       firebase.initializeApp(firebaseConfig);
+    //     }
+    //     console.log('useEffect do app');
+    //   }, []);
 
 
 
